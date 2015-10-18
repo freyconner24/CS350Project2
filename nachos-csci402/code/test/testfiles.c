68f33022ad38be9ff94e175b9abfbb32b929461a
Yui Kwan Calvin Leung <yuikwanl@usc.edu>
2015-10-18 00:12:32 -0700

    merge

--- nachos-csci402/code/test/testfiles.c
+++ nachos-csci402/code/test/testfiles.c
@@ -12,17 +12,18 @@ void helloWorld(){
 int main() {
 	OpenFileId fd;
 	int bytesread, lockNum, condNum;
-	char buf[20];
+ 	char buf[20];
 
 	Write("Testing Locks\n", 14, ConsoleOutput);
+  /*Write("Testing Locks\n", 14, ConsoleOutput);
 
-	lockNum = CreateLock("nameLock");
-	Acquire(lockNum);
-	condNum = CreateCondition("someCondition");
-	Signal(lockNum, condNum);
-	Broadcast(lockNum, condNum);
-	Release(lockNum);
-	DestroyLock(lockNum);
-	Write("Locks complete\n", 15, ConsoleOutput);
-/*	Exec('halt');*/
+ 	lockNum = CreateLock("nameLock");
+ 	Acquire(lockNum);
+ 	Release(lockNum);
+ 	DestroyLock(lockNum);
+ 	Write("Locks complete\n", 15, ConsoleOutput);
+ 	*/
+ Fork(helloWorld);
+ /*	Exec('halt');*/
+ }
 }
